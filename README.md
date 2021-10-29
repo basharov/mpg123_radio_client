0ef631318cfaf9410a6fdf85de78186ab25038d6
Sergei Basharov <sergei.basharov@unagi.ch>
2021-10-29 19:17:57 +0300

    added test gpio script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 https://www.rubydoc.info/gems/mpg123_remote/0.0.1
 https://github.com/georgi/mpg123/blob/master/doc/README.remote
+https://docs.armbian.com/User-Guide_Armbian-Config/
+https://github.com/orangepi-xunlong/wiringOP
+https://www.npmjs.com/package/orange-pi-gpio
---
+++ test_gpio.js
@@ -0,0 +1,21 @@
+const Gpio = require('orange-pi-gpio');
+
+let gpio5 = new Gpio({
+  pin: 5, mode: 'out', ready: () => {
+    let value = 1;
+
+    setInterval(function () {
+      process.stdout.write('\x1B[2J\x1B[0f\u001b[0;0H');
+
+      if (value) {
+        console.log('\x1b[32m%s\x1b[0m', `ON`);
+      } else {
+        console.log('\x1b[31m%s\x1b[0m', `OFF`);
+      }
+
+      gpio5.write(value);
+      value = +!value;
+    }, 50);
+
+  }
+});
