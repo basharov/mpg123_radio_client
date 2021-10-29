be06a3497fb83d576bf3ad379df49e51ccf78df3
Sergei Basharov <sergei.basharov@unagi.ch>
2021-10-29 19:35:23 +0300

    added test gpio script

--- test_gpio.js
+++ test_gpio.js
@@ -1,20 +1,8 @@
 const Gpio = require('orange-pi-gpio');
 
-let gpio5 = new Gpio({
-  pin: 5, mode: 'out', ready: () => {
-    let value = 1;
+let gpio5 = new Gpio({pin:5});
 
-    setInterval(function () {
-
-      if (value) {
-        console.log('ON');
-      } else {
-        console.log('OFF');
-      }
-
-      gpio5.write(value);
-      value = +!value;
-    }, 1000);
-
-  }
-});
+gpio5.read()
+  .then((state)=>{
+    console.log(state); //state of pin 5
+  });
