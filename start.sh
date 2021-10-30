be2886af5c45755e479a50f05adebde7b6ec5916
Sergei Basharov <sergei.basharov@unagi.ch>
2021-10-30 15:04:29 +0300

    radio config

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mpg123 -o alsa:hw:1:0 -v --remote --fifo /tmp/mpg123in 2> /dev/null > /tmp/mpg123out &
+mpg123 -o alsa:hw:1,0 -v --remote --fifo /tmp/mpg123in 2> /dev/null > /tmp/mpg123out &
