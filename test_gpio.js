52ba0be944dae4194d58f5b15a43602d86b9071a
Sergei Basharov <sergei.basharov@unagi.ch>
2021-10-30 15:10:09 +0300

    radio config

--- test_gpio.js
+++ test_gpio.js
@@ -9,15 +9,15 @@ let value = 0;
 
 let gpio1 = new Gpio({pin: 1, mode: 'in'});
 
-const switchChannel = () => {
-  exec('./play.sh 1', (err, stdout, stderr) => {
+const switchChannel = (value) => {
+  exec(`./play.sh ${value}`, (err, stdout, stderr) => {
     if (err) {
       //some err occurred
       console.error(err)
     } else {
       // the *entire* stdout and stderr (buffered)
-      // console.log(`stdout: ${stdout}`);
-      // console.log(`stderr: ${stderr}`);
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
     }
   });
 }
