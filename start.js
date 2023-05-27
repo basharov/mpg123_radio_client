c5a7a5c40dda93a03ba04880d14034ed0171f674
Sergei Basharov <sergei@basharov.net>
2023-05-27 21:18:55 +0300

    stations switch

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ const myEncoder = nodaryEncoder(20, 9);
 
 let previousStation = 0
 let station = 0
+let rotationValue = 0
 
 const stationsTitles = [
   'Vaporwaves',
@@ -16,18 +17,28 @@ const stations = [
   'http://somafm.com/m3u/bootliquor.m3u',
   'http://somafm.com/m3u/indiepop.m3u',
   'http://somafm.com/m3u/groovesalad.m3u',
+  'http://somafm.com/m3u/u80s.m3u',
+  'http://somafm.com/m3u/7soul.m3u',
+  'http://somafm.com/m3u/lush.m3u',
 ]
+
+player.openPlaylist(stations[station], {
+  cache: 128,
+  cacheMin: 1
+})
 
 myEncoder.on('rotation', (direction, value) => {
   previousStation = station
 
-  if (value > 3) {
-    station = 3
+  if (value > stations.length - 1) {
+    station = stations.length - 1
   } else if (value < 0) {
     station = 0
   } else {
     station = value
   }
+
+  rotationValue = value
 
 
   if (station !== previousStation) {
